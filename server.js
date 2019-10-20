6269ba1f67fb3120af2b0a271a35b936344435d9
bryceparkman <brycecparkman@gmail.com>
2019-10-20 14:58:29 -0400

    Removed socket.io (unused)

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var socket = require('socket.io')
 
 var app = express();
 var server = app.listen(3000);
@@ -7,10 +6,4 @@ var server = app.listen(3000);
 console.log("Running...")
 
 app.use(express.static('public'));
-
-var io = socket(server);
-
-io.sockets.on('connection', (socket) => {
-	console.log('Socket ' + socket.id + ' connected')
-})
 
