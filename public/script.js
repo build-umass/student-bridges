4ed31d6788f81e350f5ae785d22de5e54a18bb43
bryceparkman <brycecparkman@gmail.com>
2019-12-08 15:08:27 -0500

    Made navbar mobile-friendly

--- public/script.js
+++ public/script.js
@@ -1,7 +1,19 @@
+let x = window.matchMedia("(max-width: 992px)");
+loadNav(x);
+x.addListener(loadNav);
 
-$(function(){
-    $("#navbar").load("navbar.html");
-});
 $(function(){
     $("#footer").load("footer.html");
 });
+
+function loadNav(x) {
+    if (x.matches) { 
+        $(function(){
+            $("#navbar").load("mobilenavbar.html");
+        });
+    } else {
+        $(function(){
+            $("#navbar").load("navbar.html");
+        });
+    }
+  }
