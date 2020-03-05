16cdd94e2d3f17c522abecfe834a7f3bd9050d2d
Nilay Sadavarte <nilay18sadavarte@gmail.com>
2020-03-05 13:20:49 -0500

    Restructure for firebase

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-
-var app = express();
-var server = app.listen(3000);
-
-console.log("Running...")
-
-app.use(express.static("public"));
-app.use('/assets', express.static(__dirname + "/assets"));
